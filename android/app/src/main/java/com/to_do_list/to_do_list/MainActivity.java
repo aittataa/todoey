bd40b9c9fc3747a02ac434f77908399d2227fb26
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-30 22:22:40 +0100

    commit

--- android/app/src/main/java/com/to_do_list/to_do_list/MainActivity.java
+++ android/app/src/main/java/com/to_do_list/to_do_list/MainActivity.java
@@ -1,4 +1,4 @@
-package com.aittataa.todoey;
+package com.to_do_list.to_do_list;
 
 import io.flutter.embedding.android.FlutterActivity;
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:todoey/app/config/messages/app_message.dart';
 
-import 'app/config/functions/app_function.dart';
+import 'package:get/get.dart';
+
 import 'app/routes/app_pages.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  AppFunction.configureDependencies;
-  runApp(Todoey());
-}
-
-class Todoey extends StatelessWidget {
-  const Todoey({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: AppMessage.appTitle,
+  runApp(
+    GetMaterialApp(
+      title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    );
-  }
+    ),
+  );
 }
