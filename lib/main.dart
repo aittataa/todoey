bcddef1c282242ef3aad308e36ac89c2d261eea9
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-12-31 15:05:57 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:to_do_list/app/routes/app_pages.dart';
+
+import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
