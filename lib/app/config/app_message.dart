05ae83a4d14e14ddb0fb7c408a381917cdfb3df6
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-30 18:06:20 +0100

    commit

--- lib/app/config/app_message.dart
+++ lib/app/config/app_message.dart
@@ -5,11 +5,8 @@ class AppMessage {
   static const String appTitle = "To-Do List";
   static const String newTask = "New Task";
 
-  /// TODO : Data
-  static const String collectionsAssets = "collections";
-
   /// TODO : App Icon
-  static const String appLogo = "assets/icons/icon.png";
-  static const String appIcon = "assets/icons/app_icon.png";
-  static const String appIconRound = "assets/icons/app_icon_round.png";
+  static const String appLogo = "assets/icon.png";
+  static const String appIcon = "assets/app_icon.png";
+  static const String appIconRound = "assets/app_icon_round.png";
 }
