e3813db9a0a98c5f92c6d5bb83b772bfc1a430a1
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-30 19:06:14 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'app/config/app_function.dart';
 import 'app/config/app_message.dart';
 import 'app/config/app_theme.dart';
-import 'app/modules/home/pages/splash.dart';
+import 'app/modules/home/views/home_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,8 +21,8 @@ class ToDoList extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: AppMessage.appTitle,
       theme: AppTheme.themeData,
-      home: Splash(),
-      //home: HomeView(),
+      // home: Splash(),
+      home: HomeView(),
     );
   }
 }
