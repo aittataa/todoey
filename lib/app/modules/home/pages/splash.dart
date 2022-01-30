b85dc6ad2bb4d46c13cc73861a17724ee9c47cf8
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-30 18:55:23 +0100

    commit

--- lib/app/modules/home/pages/splash.dart
+++ lib/app/modules/home/pages/splash.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
 import '../../../config/app_constant.dart';
+import '../../../config/app_message.dart';
 import '../../../config/app_theme.dart';
 import '../views/home_view.dart';
 import '../widgets/bounce_point.dart';
@@ -11,13 +12,19 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      splash: BouncePoint(size: 60),
+      splash: Column(
+        children: [
+          Expanded(flex: 3, child: Image.asset(AppMessage.appLogo, width: 200, height: 200)),
+          Expanded(child: BouncePoint(size: 60)),
+        ],
+      ),
       nextScreen: HomeView(),
       curve: AppConstant.curve,
       backgroundColor: AppTheme.primaryBackColor,
       splashTransition: SplashTransition.slideTransition,
       pageTransitionType: PageTransitionType.rightToLeftWithFade,
       animationDuration: AppConstant.durationSplash,
+      splashIconSize: double.infinity,
     );
   }
 }
