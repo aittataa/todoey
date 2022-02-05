3965ae57e1d9b3e5979f714dc791e9d454883468
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-02-05 21:03:56 +0100

    commit

--- lib/app/modules/home/pages/home.dart
+++ lib/app/modules/home/pages/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:to_do_list/app/modules/home/widgets/collection_shape.dart';
 
 import '../../../config/app_function.dart';
 import '../../../config/app_message.dart';
@@ -40,112 +39,17 @@ class _HomeState extends State<Home> {
             return BouncePoint();
           } else {
             final List<Collection> collections = controller.collections;
-            // final List<Collection> myList = collections..sort((a, b) => b.id!.compareTo(a.id!));
             final bool isEmpty = collections.isEmpty;
             if (isEmpty) {
               return EmptyBox();
             } else {
-              return HomeBody(
+              return Body(
                 controller: controller,
                 myList: collections..sort((a, b) => b.id!.compareTo(a.id!)),
               );
             }
           }
         }),
-      ),
-    );
-  }
-}
-
-class HomeBody extends StatelessWidget {
-  final HomeController controller;
-  final List<Collection> myList;
-  const HomeBody({Key? key, required this.controller, required this.myList}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    /*return GroupListView(
-      sectionsCount: myList.length,
-      countOfItemInSection: (section) {
-        return myList.length;
-      },
-      groupHeaderBuilder: (context, index) {
-        return Text(
-          myList[index].date.toString(),
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-        );
-      },
-      itemBuilder: (context, index) {
-        return Text(
-          myList[index.section].title!,
-          style: TextStyle(color: Colors.red, fontSize: 18),
-        );
-      },
-      separatorBuilder: (context, index) => SizedBox(height: 10),
-      sectionSeparatorBuilder: (context, section) => SizedBox(height: 10),
-    );*/
-    /*return StickyGroupedListView<dynamic, DateTime>(
-      elements: myList,
-      groupBy: (collection) => collection.date,
-      groupSeparatorBuilder: (date) => GroupSeparator(date: date),
-      order: StickyGroupedListOrder.ASC, // optional
-      itemBuilder: (context, dynamic collection) => Text(collection.title!),
-      // itemComparator: (a, b) => b.title!.compareTo(a.title!), // optional
-      // useStickyGroupSeparators: true, // optional
-      floatingHeader: true, // optional
-      //
-    );*/
-
-    /*return GroupedListView<dynamic, DateTime>(
-      elements: myList,
-      groupBy: (collection) => collection.date,
-      groupSeparatorBuilder: (DateTime date) => GroupSeparator(date: date),
-      order: GroupedListOrder.ASC, // optional
-      itemBuilder: (context, dynamic collection) => Text(collection.title!),
-      itemComparator: (a, b) => b.date!.compareTo(a.date!), // optional
-      useStickyGroupSeparators: true, // optional
-      floatingHeader: true, // optional
-      //
-    );*/
-
-    return ListView.builder(
-      shrinkWrap: true,
-      padding: EdgeInsets.all(10),
-      scrollDirection: Axis.vertical,
-      physics: BouncingScrollPhysics(),
-      itemCount: myList.length,
-      itemBuilder: (context, i) {
-        final Collection collection = myList[i];
-        return CollectionShape(
-          controller: controller,
-          collection: collection,
-          onPressed: () async {
-            final int id = collection.id!;
-            final data = await controller.deleteCollection(id);
-            // setState(() {
-            print(myList.remove(collection));
-            print(data);
-            // });
-          },
-        );
-      },
-    );
-  }
-}
-
-class GroupSeparator extends StatelessWidget {
-  final DateTime date;
-  const GroupSeparator({Key? key, required this.date}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.only(left: 16, top: 8, bottom: 8),
-        child: Text(
-          "${this.date.day}/${this.date.month}/${this.date.year}",
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
       ),
     );
   }
---
+++ lib/app/modules/home/widgets/home_body.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/app/modules/home/controllers/home_controller.dart';
+import 'package:to_do_list/app/modules/home/models/collection.dart';
+
+import 'collection_shape.dart';
+
+class Body extends StatefulWidget {
+  final HomeController controller;
+  final List<Collection> myList;
+  const Body({Key? key, required this.controller, required this.myList}) : super(key: key);
+  @override
+  State<Body> createState() => _BodyState(controller, myList);
+}
+
+class _BodyState extends State<Body> {
+  final HomeController controller;
+  final List<Collection> myList;
+  _BodyState(this.controller, this.myList);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      padding: EdgeInsets.all(10),
+      scrollDirection: Axis.vertical,
+      physics: BouncingScrollPhysics(),
+      itemCount: myList.length,
+      itemBuilder: (context, i) {
+        final Collection collection = myList[i];
+        return CollectionShape(
+          controller: controller,
+          collection: collection,
+          onPressed: () async {
+            final int id = collection.id!;
+            final data = await controller.deleteCollection(id);
+            setState(() {
+              print(myList.remove(collection));
+              print(data);
+            });
+          },
+        );
+      },
+    );
+  }
+}
