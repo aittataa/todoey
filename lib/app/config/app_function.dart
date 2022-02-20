484a87d8861b0507ae589927bd60fb27cf5bafe9
aittataabderrahim <aittata.abderrahim@gmail.com>
2022-02-20 17:41:06 +0100

    commit

--- lib/app/config/app_function.dart
+++ lib/app/config/app_function.dart
@@ -107,9 +107,9 @@ class AppFunction {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
-        // systemNavigationBarColor: AppTheme.primaryBackColor,
-        // systemNavigationBarDividerColor: AppTheme.primaryBackColor,
-        // systemNavigationBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: AppTheme.transparentColor,
+        systemNavigationBarDividerColor: AppTheme.transparentColor,
+        systemNavigationBarIconBrightness: Brightness.dark,
         statusBarColor: AppTheme.transparentColor,
         statusBarIconBrightness: Brightness.dark,
         statusBarBrightness: Brightness.dark,
